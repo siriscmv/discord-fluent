f69e95b7a63c0144c3625bd456402c45400ef3b3
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-01-02 21:33:21 +0530

    fix: few emojis

--- index.mjs
+++ index.mjs
@@ -1,40 +1,43 @@
 const TWEMOJI_URL = "https://emojis.wiki/twitter/";
 import fetch from "node-fetch";
-import fs from "fs";
+import { writeFileSync } from "fs";
 
-const toFluent = (emoji) => `https://api.iconify.design/fluent-emoji/${emoji}.svg`;
-const toCSS = (emoji) => `img[aria-label*="${emoji.emoji}"] { content: url("${emoji.url}"); }\n`
+const toFluent = (emoji) =>
+    `https://api.iconify.design/fluent-emoji/${emoji}.svg`;
+const toCSS = (emoji) =>
+    `img[aria-label*="${emoji.emoji}"] { content: url("${emoji.url}"); }\n`;
+
+const strip = (name) => {
+    return name
+        .replaceAll(":", "")
+        .replaceAll("“", "")
+        .replaceAll("”", "")
+        .replaceAll("’", "")
+        .replaceAll("️", "")
+        .replace(/^tok$/g, "");
+};
 
 const getEmojis = async () => {
     const response = await fetch(TWEMOJI_URL);
     const text = await response.text();
-    const emojis = text.match(/(?<=alt=")(.*)(?=")/g).map(e => {
-        const text = e.split("\"")[0].split(" ");
+    const emojis = text.match(/(?<=alt=")(.*)(?=")/g).map((e) => {
+        const text = e.split('"')[0].split(" ");
         const emoji = text.shift();
-        const name = text.map(s => s.toLowerCase()).join("-").replace("keycap:-", "keycap-");
+        const name = strip(text.map((s) => s.toLowerCase()).join("-"));
 
-        if (name === "") return {emoji: null, url: null};
-        return {emoji, url: toFluent(name)};
+        if (name === "") return { emoji: null, url: null };
+        return { emoji, url: toFluent(name) };
     });
 
     return emojis;
-}
+};
 
-const generateCSS = (emojis) => {
-    let css = "";
-    for (const emoji of emojis) {
-        if (emoji.emoji === null) continue;
-        css += toCSS(emoji);
-    }
+const generateCSS = (emojis) =>
+    emojis
+        .filter((e) => e.emoji)
+        .map(toCSS)
+        .join("");
 
-    return css;
-}
-
-const saveFile = (css) => {
-    fs.writeFile("emojis.css", css, (err) => {
-        if (err) throw err;
-        console.log("The file has been saved!");
-    });
-}
+const saveFile = (css) => writeFileSync("emojis.css", css);
 
 getEmojis().then(generateCSS).then(saveFile);
